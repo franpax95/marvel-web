0a48881eac8fc07db2a7710d65c6ac1874dad2e2
Franpax <fcojnavarrowork@gmail.com>
2023-04-02 10:31:16 +0200

    Marvel service implementation for characters

---
+++ src/modules/main.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from './components.module';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        FormsModule,
+        ComponentsModule
+    ],
+    exports: [],
+})
+export class MainModule { }
